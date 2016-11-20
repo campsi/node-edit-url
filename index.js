436bd3c9bf22f1e8c07ff3a5330bb78b1a4d5555
romainbessugesmeusy <romainbessuges@gmail.com>
2016-11-20 12:16:04 +0100

    JSDoc

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 const {parse, format} = require('url');
-
-module.exports = function editURL(subject, fn, cb) {
-    const parsed = parse(subject, true, true);
+/**
+ * @name UrlObject
+ * @type {Object}
+ * @property {String} protocol
+ * @property {String|Number} port
+ * @property {String} hostname
+ * @property {String} pathname
+ * @property {String} hash
+ * @property {Object} query
+ * @property {String} auth
+ */
+/**
+ * @callback editFunction
+ * @param {UrlObject} obj
+ * @param {Function} [cb] if editURL is called async
+ */
+/**
+ *
+ * @param {String} url
+ * @param {editFunction} fn
+ * @param {Function} [cb] Optional : if specified, a callback is passed to the modifyFunction
+ */
+module.exports = function editURL(url, fn, cb) {
+    const parsed = parse(url, true, true);
     let obj = JSON.parse(JSON.stringify(parsed));
     delete obj.host;
     delete obj.path;
@@ -10,7 +31,6 @@ module.exports = function editURL(subject, fn, cb) {
     if (typeof cb === 'function') {
         fn(obj, ()=>cb(format(obj)));
     } else {
-        console.dir(obj);
         fn(obj);
         return format(obj);
     }
