2d800ca8c87b173b62fb821f5287a4e437bb8d79
romainbessugesmeusy <romainbessuges@gmail.com>
2016-11-20 12:21:48 +0100

    jsdoc

--- index.js
+++ index.js
@@ -2,21 +2,22 @@ const {parse, format} = require('url');
 /**
  * @name UrlObject
  * @type {Object}
- * @property {String} protocol
- * @property {String|Number} port
- * @property {String} hostname
- * @property {String} pathname
- * @property {String} hash
- * @property {Object} query
- * @property {String} auth
+ * @property {String} protocol https
+ * @property {String|Number} port 8080
+ * @property {String} hostname www.example.com
+ * @property {String} pathname admin/articles
+ * @property {String} hash #example
+ * @property {Object} query {"q": "search"}
+ * @property {String} auth admin:password
  */
 /**
  * @callback editFunction
  * @param {UrlObject} obj
  * @param {Function} [cb] if editURL is called async
  */
+
+
 /**
- *
  * @param {String} url
  * @param {editFunction} fn
  * @param {Function} [cb] Optional : if specified, a callback is passed to the modifyFunction
